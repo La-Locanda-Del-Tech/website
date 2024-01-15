01e2c7a1910ea99cc40928f3a0a7f94ed94f8da3
Pana <vip_P4na@protonmail.com>
2024-01-15 22:47:31 +0100

    chore: create first main test

--- test/main.test.tsx
+++ test/main.test.tsx
@@ -1,5 +1,11 @@
-import { test, expect } from "vitest";
+import { describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Page from "../app/page";
 
-test("true should be true", () => {
-  expect(true).toBe(true);
+describe("homepage", () => {
+  it("should render the component and show La locanda del tech", () => {
+    render(<Page />);
+    const homescreenTitleId = screen.getByTestId("homescreen-title");
+    expect(homescreenTitleId).toBeDefined();
+  });
 });
