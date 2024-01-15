cecb15c36b8f327aac0f0ca54f99fc15a5005717
Pana <Vip_P4na@protonmail.com>
2024-01-15 22:54:08 +0100

    create first test (#3)
    * chores: configuration first test
    
    * chore: create first main test
    
    * chore: format and lint files

---
+++ test/main.test.tsx
@@ -0,0 +1,11 @@
+import { describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Page from "../app/page";
+
+describe("homepage", () => {
+	it("should render the component and show La locanda del tech", () => {
+		render(<Page />);
+		const homescreenTitleId = screen.getByTestId("homescreen-title");
+		expect(homescreenTitleId).toBeDefined();
+	});
+});
